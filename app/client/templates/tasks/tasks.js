bed9e5719b7d8c27fd6507219a83ed4476b17803
Justin Tanacredi <jtanacredi@gmail.com>
2015-05-21 16:24:06 -0400

    Move task functions into Meteor methods

--- app/client/templates/tasks/tasks.js
+++ app/client/templates/tasks/tasks.js
@@ -1,78 +1,62 @@
-  Template.task.helpers({
-    tasks: function () {
-        console.log('currentUser: ' + Meteor.userId());
-	      return Tasks.find({owner: Meteor.userId(), checked: {$ne: true}}, {sort: {createdAt: -1}});
-	  },
-		completedTasks: function () {
-			return Tasks.find({owner: Meteor.userId(), checked: {$ne: false}}, {sort: {createdAt: -1}});
+Template.task.helpers({
+	tasks: function () {
+			console.log('currentUser: ' + Meteor.userId());
+			return Tasks.find({owner: Meteor.userId(), checked: {$ne: true}}, {sort: {createdAt: -1}});
+	},
+	completedTasks: function () {
+		return Tasks.find({owner: Meteor.userId(), checked: {$ne: false}}, {sort: {createdAt: -1}});
+	}
+});
+
+Template.taskItem.helpers({
+	owner: function (id) {
+		console.log('id: ' + id);
+		return Meteor.users.findOne(id).profile.name;
+	}
+});
+
+Template.task.events({
+	"submit .new-task": function (event) {
+
+		var taskItem = event.target.text.value;
+
+		Meteor.call("addTask", taskItem);
+		
+		event.target.text.value = "";
+
+		return false;
+//   },
+//   "change .hide-completed input": function(event) {
+//     Session.set("hideCompleted", event.target.checked);
+	},
+	"click .toggleComplete": function (event) {
+
+			var thisclick = $(this).get(0);
+			console.log(thisclick);
+		return false;
+	},
+	"click .toggleComplete": function() {
+		Meteor.call("setChecked", this._id, ! this.checked);
+	}
+
+
+});
+
+Meteor.methods({
+	addTask: function (text) {
+		if (! Meteor.userId()) {
+			throw new Meteor.Error("not-authorized");
 		}
-  });
-
-  Template.taskItem.helpers({
-    owner: function (id) {
-      console.log('id: ' + id);
-      return Meteor.users.findOne(id).profile.name;
-    }
-  });
-
-  Template.task.events({
-    "submit .new-task": function (event) {
-    
-      var taskItem = event.target.text.value;
-
-      Tasks.insert({
-        text: taskItem,
-        createdAt: new Date(),
-        owner: Meteor.userId()
-      });
-        event.target.text.value = "";
-
-      return false;
-  //   },
-  //   "change .hide-completed input": function(event) {
-  //     Session.set("hideCompleted", event.target.checked);
-    },
-    "click .toggleComplete": function (event) {
-
-        var thisclick = $(this).get(0);
-        console.log(thisclick);
-        // thisclick.toggleClass('complete');
-
-        // console.log(event.target);
-      return false;
-    },
-		"click .toggleComplete": function() {
-			Tasks.update(this._id, {$set: {checked: ! this.checked}});
-		}
-
-
-  });
-
-  // Template.task.events({
-  //   "click .toggle-checked": function() {
-  //     Tasks.update(this._id, {$set: {checked: ! this.checked}});
-  //   },
-  //   "click .delete": function() {
-  //     Tasks.remove(this._id);
-  //   }
-  // });
-  // Accounts.ui.config({
-  //   passwordSignupFields: "USERNAME_ONLY"
-  // });
-
-
-// Template.body.helpers({
-//     tasks: function() {
-//       if (Session.get('hideCompleted')) {
-//         return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-//       } else {
-//         return Tasks.find({}, {sort: {createdAt: -1}});
-//       } 
-//     },
-//     hideCompleted: function () {
-//       return Session.get("hideCompleted");
-//     },
-//     incompleteCount: function () {
-//       return Tasks.find({checked: {$ne: true}}).count();
-//     }
-//   });
+		
+		Tasks.insert({
+			text: taskItem,
+			createdAt: new Date(),
+			owner: Meteor.userId()
+		});
+	},
+	deleteTask: function (taskId) {
+	},
+	setChecked: function (taskId, setChecked) {
+		Tasks.update(this._id, {$set: {checked: ! this.checked}});
+	}
+});
