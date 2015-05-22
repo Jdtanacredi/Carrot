5922552b0c975126038bce73ccdcddf1e7562267
Justin Tanacredi <jtanacredi@gmail.com>
2015-05-22 11:30:55 -0400

    Tasks - Update checkbox to boolean

--- app/client/templates/tasks/tasks.js
+++ app/client/templates/tasks/tasks.js
@@ -4,9 +4,10 @@ Template.task.helpers({
 	tasks: function () {
 			console.log('currentUser: ' + Meteor.userId());
 			return Tasks.find({owner: Meteor.userId(), checked: {$ne: true}}, {sort: {createdAt: -1}});
+			//return Tasks.find({owner: Meteor.userId(), checked: 0}, {sort: {createdAt: -1}});
 	},
 	completedTasks: function () {
-		return Tasks.find({owner: Meteor.userId(), checked: {$ne: false}}, {sort: {createdAt: -1}});
+		return Tasks.find({owner: Meteor.userId(), checked:  1}, {sort: {createdAt: -1}});
 	}
 });
 
@@ -22,31 +23,19 @@ Template.task.events({
 		var taskItem = event.target.text.value;
 		Meteor.call("addTask", taskItem);
 		event.target.text.value = "";
-		return false;		
-//   },
-//   "change .hide-completed input": function(event) {
-//     Session.set("hideCompleted", event.target.checked);
-	},
-	"click .toggleComplete": function (event) {
-
-			var thisclick = $(this).get(0);
-			console.log(thisclick);
 		return false;
 	},
-	"click .toggleComplete": function() {
+	"click .toggleComplete": function (event) {
 		var thisclick = $(this).get(0);
-		console.log(thisclick);
 		Meteor.call("setChecked", this._id, ! this.checked);
 	}
-
-
 });
 
 Meteor.methods({
 	deleteTask: function (taskId) {
-	},
-	setChecked: function (taskId) {
-		Tasks.update(this._id, {$set: {checked: ! this.checked}});
-		console.log(this);
-	}
+	}//,
+//	setChecked: function (taskId) {
+//		Tasks.update(this._id, {$set: {checked: ! this.checked}});
+//		console.log(this);
+//	}
 });
--- app/server/tasks.js
+++ app/server/tasks.js
@@ -9,39 +9,29 @@ Meteor.methods({
 		if (! Meteor.userId()) {
 			throw new Meteor.Error("not-authorized");
 			return false;
-		}
-		
+		}		
 		Tasks.insert({
 			text: text,
 			createdAt: new Date(),
 			owner: Meteor.userId(),
-			checked: false
+			checked: 0
 		});
 		return false;
 	},
 	deleteTask: function (taskId) {
 	},
-	setChecked: function (taskId) {
-		Tasks.update(this._id, {$set: {checked: ! this.checked}});
-		console.log(this);
+	setChecked: function (taskId, checked) {
+		console.log('checked');
+		Tasks.update(this._id, {$set: {checked: ! checked}});
+		// (query, {$inc: {flag: 1}}) LOOK INTO THIS:     http://stackoverflow.com/questions/10506878/how-to-toggle-a-boolean-field-in-an-array-element-in-mongodb
 	}
 });
 
-
-  Meteor.startup(function() {
-
-    return Meteor.methods({
-
-      removeAllTasks: function() {
-
-        return Tasks.remove({});
-
-      }
-
-    });
-
+Meteor.startup(function() {
+  return Meteor.methods({
+    removeAllTasks: function() {
+			return Tasks.remove({});
+				// CALL FUNCTION WITH Meteor.call('removeAllTasks'); in inspector console. Will remove all db tasks
+    }
   });
-
-  // CALL ABOVE FUNCTION WITH
-
-  // Meteor.call('removeAllTasks')
+});
