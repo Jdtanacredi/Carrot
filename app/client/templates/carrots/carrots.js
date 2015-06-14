30d81a3b76bbf9889f097f0049045c97e9648c5b
Justin Tanacredi <jtanacredi@gmail.com>
2015-06-14 16:20:43 -0400

    Carrot - Create images collection.

--- app/client/templates/carrots/carrots.js
+++ app/client/templates/carrots/carrots.js
@@ -1,5 +1,12 @@
 Meteor.subscribe("carrots");
 
+var imageStore = new FS.Store.GridFS("images");
+
+Images = new FS.Collection("images", {
+ stores: [imageStore]
+});
+
+
 Template.carrotList.helpers({
 	carrots: function () {
 		return Carrots.find({owner: Meteor.userId()}, {sort: {createdAt: -1}});
@@ -25,12 +32,30 @@ console.log(taskIds);
 Template.carrotList.events({
 	"submit .new-carrot": function(event) {
 		var carrotReward = event.target.text.value;
-//		var associatedTasks = $( ".taskSelect").val();
 		var associatedTasks = $(".associatedTasks:checked").map(function() {
 			return this.value;
 		}).get();
-//		console.log(associatedTasks);
 		Meteor.call("addCarrot", carrotReward, associatedTasks);
 		return false;
-	}
+	},
+// MOVE TO SERVER. ONCE FUNCTIONING, MOVE TO ON SUBMIT
+	'change .myFileInput': function(event, template) {
+      FS.Utility.eachFile(event, function(file) {
+        Images.insert(file, function (err, fileObj) {
+          if (err){
+						console.log(err);
+             // handle error
+          } else {
+             // handle success depending what you need to do
+            var userId = Meteor.userId();
+            var imagesURL = {
+              'profile.image': '/cfs/files/images/' + fileObj._id
+            };
+            //Meteor.users.update(userId, {$set: imagesURL});
+          }
+        });
+     });
+   }
 });
+
+
